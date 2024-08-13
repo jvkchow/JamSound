92168fa60b0b567ee806c6b7d49bc84d067ecaac
jvkchow <jechow@ualberta.ca>
2024-08-12 20:36:18 -0600

    set up initial home page

--- jamsound/jamsound/urls.py
+++ jamsound/jamsound/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from search.views import SearchView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', SearchView.as_view(template_name='home.html'), name='search')
 ]
