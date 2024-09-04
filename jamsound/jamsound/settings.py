102fa504560931d4784b7d954b1602586752ce53
jvkchow <jechow@ualberta.ca>
2024-09-04 15:57:35 -0600

    added vercel connection services

--- jamsound/jamsound/settings.py
+++ jamsound/jamsound/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8g88%7&uq1(%*eg-toc*fmm+n@j_($tfs+i)wa*9%&4g-@gjf2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
