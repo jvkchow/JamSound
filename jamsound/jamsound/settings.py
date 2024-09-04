1a7aaa2eb550a4ff0122339c53b68a9bb80b342c
jvkchow <jechow@ualberta.ca>
2024-09-04 17:40:42 -0600

    added vercel runtime python version

--- jamsound/jamsound/settings.py
+++ jamsound/jamsound/settings.py
@@ -15,7 +15,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +56,7 @@ ROOT_URLCONF = 'jamsound.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(SETTINGS_PATH, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
