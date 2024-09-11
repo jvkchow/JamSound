4370a3a4aea9955a6d9ad5088d3bc81e6911cf03
jvkchow <jechow@ualberta.ca>
2024-09-11 17:35:39 -0600

    added changes to deploy on vercel

--- jamsound/jamsound/settings.py
+++ jamsound/jamsound/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8g88%7&uq1(%*eg-toc*fmm+n@j_($tfs+i)wa*9%&4g-@gjf2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
