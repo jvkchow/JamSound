985d6505326abf769b09592946b49952da6f99c1
jvkchow <jechow@ualberta.ca>
2024-09-11 17:29:03 -0600

    changed vercel src

--- jamsound/jamsound/settings.py
+++ jamsound/jamsound/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8g88%7&uq1(%*eg-toc*fmm+n@j_($tfs+i)wa*9%&4g-@gjf2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
