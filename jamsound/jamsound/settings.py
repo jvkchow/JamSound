a1cd0c4108f5f858f66a1e2833cebc60c17b9596
jvkchow <jechow@ualberta.ca>
2024-09-11 16:58:57 -0600

    changed a base directory for templates

--- jamsound/jamsound/settings.py
+++ jamsound/jamsound/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'jamsound.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'jamsound/templates/')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'jamsound/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
