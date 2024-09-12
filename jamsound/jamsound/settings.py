c5f4a86ae34b2628e292fb39c478456c0d3970c2
jvkchow <jechow@ualberta.ca>
2024-09-11 18:04:30 -0600

    changed static files settings

--- jamsound/jamsound/settings.py
+++ jamsound/jamsound/settings.py
@@ -121,12 +121,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    "res/"
-]
-
-MEDIA_URL = 'res/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'res')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- jamsound/jamsound/urls.py
+++ jamsound/jamsound/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home')
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
