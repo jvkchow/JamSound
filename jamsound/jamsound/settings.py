e3c0800d0e0861d058ee8236d2a8e179ed3effcb
jvkchow <jechow@ualberta.ca>
2024-09-11 21:16:15 -0600

    test remove base.html

--- jamsound/jamsound/settings.py
+++ jamsound/jamsound/settings.py
@@ -54,8 +54,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'jamsound.urls'
 
-print(os.path.join(BASE_DIR, 'templates'))
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
