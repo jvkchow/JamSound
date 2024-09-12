6a426379f22098e10b017abb8eac8ec5619700b5
jvkchow <jechow@ualberta.ca>
2024-09-11 18:28:26 -0600

    reversed static changes

--- jamsound/jamsound/settings.py
+++ jamsound/jamsound/settings.py
@@ -121,8 +121,12 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = [
+    "res/"
+]
+
+MEDIA_URL = 'res/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'res')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- jamsound/jamsound/urls.py
+++ jamsound/jamsound/urls.py
@@ -25,5 +25,4 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home')
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
