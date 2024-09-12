202ed7c0b0dfd925cde0a9a5c5fb5ddec9c3371c
jvkchow <jechow@ualberta.ca>
2024-09-11 23:17:37 -0600

    made changes for static files

--- jamsound/jamsound/settings.py
+++ jamsound/jamsound/settings.py
@@ -119,12 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    "res/"
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'res')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'res')
 
 if not DEBUG:
     STATCFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
