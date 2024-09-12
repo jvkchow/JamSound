2b8ead22f9fc463d03b8179c626cc3f5445fcd51
jvkchow <jechow@ualberta.ca>
2024-09-11 23:50:50 -0600

    static changes

--- jamsound/jamsound/settings.py
+++ jamsound/jamsound/settings.py
@@ -119,14 +119,16 @@ USE_TZ = True
 # ]
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # if not DEBUG:
 #     STATCFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = 'res/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'res')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
